5ebea1e1e4c4eeb55d380810de90abc420306e3d
marcomelesio <120285789+marcomelesio@users.noreply.github.com>
2022-12-13 03:08:16 -0500

    Add files via upload

---
+++ js/oscil.js
@@ -0,0 +1,6 @@
+const startAudio = document.querySelector("#startAudio");
+
+const playBtn = document.querySelector("#playBtn"); // Global Variable
+const stopBtn = document.querySelector("#stopBtn"); // Global Variable
+const playBtn2 = document.querySelector("#playBtn2"); // Global Variable
+const stopBtn2 = document.querySelector("#stopBtn2"); // Global Variable
